99ea7f66a9aeb40ab9efd028cf9e950888f07f4d
Shivam Srivastava <shivsri1401@gmail.com>
2025-08-24 22:45:50 +0530

    [UPD] models and create API

--- app/main.py
+++ app/main.py
@@ -1,29 +1,43 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, UploadFile, File, Form
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
 from . import models, crud, database, schemas
 import logging
 logging.basicConfig(level=logging.INFO)
 _logger = logging.getLogger(__name__)
+import os, shutil
+
 # Create database tables
 models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI()
+UPLOAD_DIR = "uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 # Dependency
 def get_db():
     db = database.SessionLocal()
-    _logger.info(f"===get_db==db=={db}===========")
     try:
         yield db
     finally:
         db.close()
 
 @app.post("/create/users/", response_model= schemas.UserResponse)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    _logger.info(f"===user=={user}===========")
-    _logger.info(f"===db=={db}===========")
-    print("hello")
-    return crud.create_user(db=db, user=user)
+def create_user(name: str= Form(...), email: str= Form(...), file: UploadFile = File(None), db: Session = Depends(get_db)):
+    user_image_path = None
+    if file:
+        file_location = os.path.join(UPLOAD_DIR, file.filename)
+        with open(file_location, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+        user_image_path = file_location
+    try:
+        return crud.create_user(db=db, user= schemas.UserCreate(name=name, email=email), user_image_path=user_image_path)
+    except IntegrityError:
+        db.rollback()  # important: reset transaction state
+        raise HTTPException(
+            status_code=400,
+            detail=f"Email '{email}' is already registered."
+        )
 
 @app.get("/get/users/", response_model=list[schemas.UserResponse])
 def read_users(offset: int = 0, limit: int = 10, db: Session = Depends(get_db)):
